c6482778ba8c54b5d846673a602efba873f47227
david85258 <marcos14david7@gmail.com>
2022-08-26 23:24:20 +0200

    Y changed

--- src/main/java/net/david85258/customores/WorldGen/CustomChunkGenerator.java
+++ src/main/java/net/david85258/customores/WorldGen/CustomChunkGenerator.java
@@ -7,6 +7,7 @@ import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.util.noise.SimplexOctaveGenerator;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -34,6 +35,6 @@ public class CustomChunkGenerator extends ChunkGenerator{
     @Override
     public List<BlockPopulator> getDefaultPopulators(World world) {
         //return Arrays.asList();
-        return Arrays.asList(new OrePopulator());
+        return Collections.singletonList(new OrePopulator());
     }
 }
--- src/main/java/net/david85258/customores/WorldGen/OrePopulator.java
+++ src/main/java/net/david85258/customores/WorldGen/OrePopulator.java
@@ -10,12 +10,11 @@ import java.util.Random;
 public class OrePopulator extends BlockPopulator {
     public void populate(World world, Random random, Chunk chunk) {
         int X, Y, Z;
-        boolean isStone;
         for (int i = 1; i < 15; i++) {  // Number of tries
             if (random.nextInt(1000) < 5) {  // The chance of spawning
                 X = random.nextInt(15);
                 Z = random.nextInt(15);
-                Y = random.nextInt(40)+20;  // Get randomized coordinates
+                Y = random.nextInt(103)-63;  // Get randomized coordinates
                 if (world.getBlockAt(X, Y, Z).getType() == Material.STONE) {
                     world.getBlockAt(X, Y, Z).setType(Material.CHEST);
                 }
