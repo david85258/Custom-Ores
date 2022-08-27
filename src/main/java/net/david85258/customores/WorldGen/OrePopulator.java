90287c0d74bf334e50aab4b85b6ecbdc4bdecf7a
david85258 <marcos14david7@gmail.com>
2022-08-27 18:41:27 +0200

    chest todos chunks

--- src/main/java/net/david85258/customores/WorldGen/OrePopulator.java
+++ src/main/java/net/david85258/customores/WorldGen/OrePopulator.java
@@ -1,8 +1,6 @@
 package net.david85258.customores.WorldGen;
 
-import org.bukkit.Chunk;
 import org.bukkit.Material;
-import org.bukkit.World;
 import org.bukkit.generator.BlockPopulator;
 import org.bukkit.generator.LimitedRegion;
 import org.bukkit.generator.WorldInfo;
@@ -11,20 +9,6 @@ import java.util.Random;
 
 @SuppressWarnings("NullableProblems")
 public class OrePopulator extends BlockPopulator {
-
-    /*public void populate(World world, Random random, Chunk chunk) {
-        int X, Y, Z;
-        for (int i = 1; i < 15; i++) {  // Number of tries
-            if (random.nextInt(1000) < 5) {  // The chance of spawning
-                X = random.nextInt(16);
-                Z = random.nextInt(16);
-                Y = random.nextInt(103)-63;  // Get randomized coordinates
-                if (chunk.getBlock(X, Y, Z).getType() == Material.STONE) {
-                    chunk.getBlock(X, Y, Z).setType(Material.CHEST);
-                }
-            }
-        }
-    }*/
 
     @Override
     public void populate(WorldInfo worldInfo, Random random, int x, int z, LimitedRegion limitedRegion) {
